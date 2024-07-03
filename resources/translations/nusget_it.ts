ca81c2e6d3b3e97342d40dfffbf2d896fc7835e1
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2024-07-04 06:13:46 +1000

    Added Norwegian translation, blanks for other common languages

--- resources/translations/nusget_it.ts
+++ resources/translations/nusget_it.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nb_NO">
+<TS version="2.1" language="it_IT">
 <context>
     <name>MainWindow</name>
     <message>
@@ -15,186 +15,177 @@ Select a title from the list on the left, or enter a Title ID to begin.
 Titles marked with a checkmark are free and have a ticket available, and can be decrypted and/or packed into a WAD or TAD. Titles with an X do not have a ticket, and only their encrypted contents can be saved.
 
 Titles will be downloaded to a folder named &quot;NUSGet&quot; inside your downloads folder.</source>
-        <translation>NUSGet v{nusget_version}
-Utviklet av NinjaCheetah
-Drevet av libWiiPy {libwiipy_version}
-DSi støtte levert av libTWLPy {libtwlpy_version}
-
-Velg en tittel fra listen til venstre, eller skriv inn en Tittel ID for å begynne.
-
-Titler merket med en hake er fri og har en billett tilgjengelig, og kan dekrypteres og/eller pakkes inn i en WAD eller TAD. Titler med en X ikke har en billett, og bare det krypterte innholdet kan lagres.
-
-Titler er lastes ned til en mappe med navnet &quot;NUSGet&quot; i nedlastingsmappen din.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../NUSGet.py" line="237"/>
         <source>Invalid Title ID</source>
-        <translation>Ugyldig Tittel ID</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../NUSGet.py" line="238"/>
         <source>The Title ID you have entered is not in a valid format!</source>
-        <translation>Tittel IDen du har angitt er ikke i et gyldig format!</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../NUSGet.py" line="240"/>
         <source>Title IDs must be 16 digit strings of numbers and letters. Please enter a correctly formatted Title ID, or select one from the menu on the left.</source>
-        <translation>Tittel IDer må være 16-sifrede tall og bokstav strenger. Vennligst skriv inn en korrekt formatert Tittel ID, eller velg en fra menyen til venstre.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../NUSGet.py" line="242"/>
         <source>Title ID/Version Not Found</source>
-        <translation>Tittel ID/Versjon Ikke Funnet</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../NUSGet.py" line="243"/>
         <source>No title with the provided Title ID or version could be found!</source>
-        <translation>Ingen tittel med oppgitt Tittel ID eller versjon ble funnet!</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../NUSGet.py" line="246"/>
         <source>Please make sure that you have entered a valid Title ID, or selected one from the title database, and that the provided version exists for the title you are attempting to download.</source>
-        <translation>Vennligst kontroller at du har oppgitt en gyldig Tittel ID, eller valgt en fra titteldatabasen, og at den angitte versjonen finnes for tittelen du forsøker å laste ned.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../NUSGet.py" line="248"/>
         <source>Content Decryption Failed</source>
-        <translation>Dekryptering av Innhold Mislyktes</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../NUSGet.py" line="249"/>
         <source>Content decryption was not successful! Decrypted contents could not be created.</source>
-        <translation>Dekryptering av innhold var ikke vellykket! Dekryptert innhold kunne ikke opprettes.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../NUSGet.py" line="252"/>
         <source>Your TMD or Ticket may be damaged, or they may not correspond with the content being decrypted. If you have checked &quot;Use local files, if they exist&quot;, try disabling that option before trying the download again to fix potential issues with local data.</source>
-        <translation>TMDen eller Billetten kan være skadet, eller det kan hende at de ikke samsvarer med innholdet some dekryptertes. Hvis du har krysset av for &quot;Bruk lokale filer, hvis de finnes&quot;, kan du prøve å deaktivere dette alternativet før du prøver nedlastingen på nytt for å løse eventuelle problemer med lokale data.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../NUSGet.py" line="255"/>
         <source>Ticket Not Available</source>
-        <translation>Billett Ikke Tilgjengelig</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../NUSGet.py" line="256"/>
         <source>No Ticket is Available for the Requested Title!</source>
-        <translation>Ikke noe billett er tilgjengelig for den forespurte tittelen!</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../NUSGet.py" line="259"/>
         <source>A ticket could not be downloaded for the requested title, but you have selected &quot;Pack installable archive&quot; or &quot;Create decrypted contents&quot;. These options are not available for titles without a ticket. Only encrypted contents have been saved.</source>
-        <translation>En billett kunne ikke lastes ned for den forespurte tittelen, men du har valgt &quot;Pakk installerbart arkiv&quot; eller &quot;Opprett dekryptert innhold&quot;. Disse alternativene er ikke tilgjenelige for titler uten billett. Bare kryptert innhold har blitt lagret.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../NUSGet.py" line="261"/>
         <source>Unknown Error</source>
-        <translation>Ukjent Feil</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../NUSGet.py" line="262"/>
         <source>An Unknown Error has Occurred!</source>
-        <translation>En ukjent feil har oppstått!</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../NUSGet.py" line="264"/>
         <source>Please try again. If this issue persists, please open a new issue on GitHub detailing what you were trying to do when this error occurred.</source>
-        <translation>Vennligst prøv igjen. Hvis dette problemet vedvarer, åpne et nytt issue på GitHub med detaljer om hva du prøvde å gjøre da denne feilen oppstod.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="26"/>
         <source>MainWindow</source>
-        <translation>MainWindow</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="41"/>
         <source>Available Titles</source>
-        <translation>Tilgjengelige Titler</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="70"/>
         <source>Wii</source>
-        <translation>Wii</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="101"/>
         <source>vWii</source>
-        <translation>vWii</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="132"/>
         <source>DSi</source>
-        <translation>DSi</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="172"/>
         <source>Title ID</source>
-        <translation>Tittel ID</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="179"/>
         <source>v</source>
-        <translation>v</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="192"/>
         <source>Version</source>
-        <translation>Versjon</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="199"/>
         <source>Console:</source>
-        <translation>Konsoll:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="227"/>
         <source>Start Download</source>
-        <translation>Start Nedlasting</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="244"/>
         <source>General Settings</source>
-        <translation>Generelle Instillinger</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="251"/>
         <source>Pack installable archive (WAD/TAD)</source>
-        <translation>Pakke installerbart arkiv (WAD/TAD)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="261"/>
         <source>File Name</source>
-        <translation>Filnavn</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="268"/>
         <source>Keep encrypted contents</source>
-        <translation>Oppbevar kryptert innhold</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="278"/>
         <source>Create decrypted contents (*.app)</source>
-        <translation>Opprette dekryptert innold (*.app)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="288"/>
         <source>Use local files, if they exist</source>
-        <translation>Bruk lokale filer, hvis de finnes</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="295"/>
         <source>Use the Wii U NUS (faster, only effects Wii/vWii)</source>
-        <translation>Bruk Wii U NUS (raskere, påvirker bare Wii/vWii)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="328"/>
         <source>vWii Title Settings</source>
-        <translation>vWii Tittelinstillinger</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="338"/>
         <source>Re-encrypt title using the Wii Common Key</source>
-        <translation>Krypter tittelen på nytt ved hjelp av Wii Common Key</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../qt/ui/MainMenu.ui" line="374"/>
@@ -203,11 +194,7 @@ Titler er lastes ned til en mappe med navnet &quot;NUSGet&quot; i nedlastingsmap
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
 &lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
-&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
-p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
