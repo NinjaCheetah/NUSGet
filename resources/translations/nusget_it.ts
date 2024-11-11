d1a811d24590708ce4915ac35df123e5759e146a
LNLenost <niccolo.salerno.2010@gmail.com>
2024-11-11 11:22:56 +0100

    Uploaded new Italian translation

---
+++ resources/translations/nusget_it.ts
@@ -0,0 +1,311 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="26"/>
+        <source>MainWindow</source>
+        <translation>Finestra principale</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="43"/>
+        <source>Available Titles</source>
+        <translation>Titoli disponibili</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="72"/>
+        <source>Wii</source>
+        <translation>Wii</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="103"/>
+        <source>vWii</source>
+        <translation>vWii</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="134"/>
+        <source>DSi</source>
+        <translation>DSi</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="174"/>
+        <source>Title ID</source>
+        <translation>ID Titolo</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="181"/>
+        <source>v</source>
+        <translation>v</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="194"/>
+        <source>Version</source>
+        <translation>Versione</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="201"/>
+        <source>Console:</source>
+        <translation>Console:</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="237"/>
+        <source>Start Download</source>
+        <translation>Avvia download</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="250"/>
+        <source>Run Script</source>
+        <translation>Avvia Script</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="277"/>
+        <source>General Settings</source>
+        <translation>Impostazioni generali</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="308"/>
+        <source>Pack installable archive (WAD/TAD)</source>
+        <translation>Archivio installabile (WAD/TAD)</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="323"/>
+        <source>File Name</source>
+        <translation>Nome del file</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="357"/>
+        <source>Keep encrypted contents</source>
+        <translation>Mantieni contenuti criptati</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="393"/>
+        <source>Create decrypted contents (*.app)</source>
+        <translation>Crea contenuto decriptato (*.app)</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="432"/>
+        <source>Use local files, if they exist</source>
+        <translation>Usa file locali, se esistenti</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="477"/>
+        <source>Use the Wii U NUS (faster, only effects Wii/vWii)</source>
+        <translation>Usa il NUS di Wii U (più veloce, riguarda solo Wii/vWii)</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="575"/>
+        <source>vWii Title Settings</source>
+        <translation>Impostazioni titoli vWii</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="609"/>
+        <source>Re-encrypt title using the Wii Common Key</source>
+        <translation>Cripta titolo usando la Chiave Comune Wii</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="666"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;meta charset=&quot;utf-8&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+hr { height: 1px; border-width: 0; }
+li.unchecked::marker { content: &quot;\2610&quot;; }
+li.checked::marker { content: &quot;\2612&quot;; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Noto Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;meta charset=&quot;utf-8&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+hr { height: 1px; border-width: 0; }
+li.unchecked::marker { content: &quot;\2610&quot;; }
+li.checked::marker { content: &quot;\2612&quot;; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Noto Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Noto Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="vanished">&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="264"/>
+        <source>No Output Selected</source>
+        <translation>Nessun output selezionato</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="265"/>
+        <source>You have not selected any format to output the data in!</source>
+        <translation>Non hai selezionato alcun formato in cui esportare i dati!</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="267"/>
+        <source>Please select at least one option for how you would like the download to be saved.</source>
+        <translation>Per favore scegli almeno un opzione per come vorresti che fosse salvato il download.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="298"/>
+        <source>Invalid Title ID</source>
+        <translation>ID Titolo invalido</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="299"/>
+        <source>The Title ID you have entered is not in a valid format!</source>
+        <translation>L&apos; ID Titolo che hai inserito non è in un formato valido!</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="301"/>
+        <source>Title IDs must be 16 digit strings of numbers and letters. Please enter a correctly formatted Title ID, or select one from the menu on the left.</source>
+        <translation>Gli ID Titolo sono un codice di 16 caratteri tra numeri e lettere. Per favore inserisci in ID Titolo formattato correttamente, o scegline uno dal menù a sinistra.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="303"/>
+        <source>Title ID/Version Not Found</source>
+        <translation>ID Titolo/Versione non trovata</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="304"/>
+        <source>No title with the provided Title ID or version could be found!</source>
+        <translation>Non è stato trovato nessun titolo con l&apos; ID Titolo o versione data!</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="306"/>
+        <source>Please make sure that you have entered a valid Title ID, or selected one from the title database, and that the provided version exists for the title you are attempting to download.</source>
+        <translation>Assicurati di aver inserito un&apos; ID Titolo valido, o scegline uno dal database, e che la versione richiesta esista per il titolo che vuoi scaricare.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="308"/>
+        <source>Content Decryption Failed</source>
+        <translation>Decriptazione contenuti fallita</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="309"/>
+        <source>Content decryption was not successful! Decrypted contents could not be created.</source>
+        <translation>La decriptazione dei contenuti non è andata a buon fine! I contenuti decriptadi non sono stati creati.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="312"/>
+        <source>Your TMD or Ticket may be damaged, or they may not correspond with the content being decrypted. If you have checked &quot;Use local files, if they exist&quot;, try disabling that option before trying the download again to fix potential issues with local data.</source>
+        <translation>Il tuo TMD o Ticket potrebbe essere danneggiato, o potrebbe non corrispondere col contenuto da decriptare. Se hai selezionato &quot;Usa file locali, se esistenti&quot;, prova a disabilitare quell&apos;opzione prima di riprovare a scaricare per aggiustare potenziali errori coi dati locali.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="315"/>
+        <source>Ticket Not Available</source>
+        <translation>Ticket non disponibile</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="316"/>
+        <source>No Ticket is Available for the Requested Title!</source>
+        <translation>Nessun ticket disponibile per il titolo richiesto!</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="319"/>
+        <source>A ticket could not be downloaded for the requested title, but you have selected &quot;Pack installable archive&quot; or &quot;Create decrypted contents&quot;. These options are not available for titles without a ticket. Only encrypted contents have been saved.</source>
+        <translation>Non è stato possibile scaricare un ticket per il titolo richiesto, ma hai selezionato &quot;Crea archivio installabile&quot; o &quot;Crea contenuto decriptato&quot;. Queste opzioni non sono disponibili per i titoli senza un ticket. Sono stati salvati solo i contenuti criptati.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="321"/>
+        <source>Unknown Error</source>
+        <translation>Errore sconosciuto</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="322"/>
+        <source>An Unknown Error has Occurred!</source>
+        <translation>Errore sconosciuto!</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="324"/>
+        <source>Please try again. If this issue persists, please open a new issue on GitHub detailing what you were trying to do when this error occurred.</source>
+        <translation>Per favore riprova. Se il problema persiste, apri un issue su GitHub specificando in modo dettagliato cosa volevi fare quando è comparso questo errore.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="364"/>
+        <source>Open NUS script</source>
+        <translation>Apri script NUS</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="364"/>
+        <source>NUS Scripts (*.nus *.txt)</source>
+        <translation>Scrpit NUS (*.nus *.txt)</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="371"/>
+        <location filename="../../NUSGet.py" line="386"/>
+        <location filename="../../NUSGet.py" line="389"/>
+        <location filename="../../NUSGet.py" line="397"/>
+        <location filename="../../NUSGet.py" line="415"/>
+        <location filename="../../NUSGet.py" line="422"/>
+        <source>Script Failure</source>
+        <translation>Errore script</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="371"/>
+        <source>Failed to open the script.</source>
+        <translation>Impossibile aprire lo script.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="85"/>
+        <source>NUSGet v{nusget_version}
+Developed by NinjaCheetah
+Powered by libWiiPy {libwiipy_version}
+DSi support provided by libTWLPy {libtwlpy_version}
+
+Select a title from the list on the left, or enter a Title ID to begin.
+
+Titles marked with a checkmark are free and have a ticket available, and can be decrypted and/or packed into a WAD or TAD. Titles with an X do not have a ticket, and only their encrypted contents can be saved.
+
+Titles will be downloaded to a folder named &quot;NUSGet&quot; inside your downloads folder.</source>
+        <translation>NUSGet v{nusget_version}
+Sviluppato da NinjaCheetah
+Funzionante con libWiiPy {libwiipy_version}
+
+Scegli un tittolo dalla lista a sinistra o inserisci un ID Titolo per iniziare.
+
+I titoli marcati da una spunta sono disponibili e hanno un ticket libero, e possono essere decriptati/scaricati come WAD o TAD. I titoli con una X non hanno un ticket e solo il contenuto criptato può essere salvato.
+
+I titoli verranno scaricati nella cartella &quot;NUSGet&quot; all&apos;interno della cartella Download.</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="519"/>
+        <source>Apply patches to IOS (Applies to WADs only)</source>
+        <translation>Applica patch agli IOS (Solo per le WAD)</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="184"/>
+        <source>NUSGet Update Available</source>
+        <translation>Aggiornamento di NUSGet disponibile</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="185"/>
+        <source>There&apos;s a newer version of NUSGet available!</source>
+        <translation>Una nuova versione di NUSGet è disponibile!</translation>
+    </message>
+    <message>
+        <location filename="../../modules/core.py" line="26"/>
+        <source>
+
+Could not check for updates.</source>
+        <translation>Impossibile trovare eventuali aggiornamenti.</translation>
+    </message>
+    <message>
+        <location filename="../../modules/core.py" line="34"/>
+        <source>
+
+There&apos;s a newer version of NUSGet available!</source>
+        <translation>Una nuova versione di NUSGet è disponibile!</translation>
+    </message>
+    <message>
+        <location filename="../../modules/core.py" line="36"/>
+        <source>
+
+You&apos;re running the latest release of NUSGet.</source>
+        <translation>Stai utilizzando l&apos;ultima versione di NUSGet.</translation>
+    </message>
+</context>
+</TS>
