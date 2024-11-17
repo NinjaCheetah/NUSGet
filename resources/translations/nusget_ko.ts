7c35e2090d6b963e07e3cc3be47f041b792b61dd
DDinghoya <ddinghoya@gmail.com>
2024-11-17 12:02:52 +0900

    Add Korean translations (#15)
    * Create nusget_ko.ts
    
    * Update NUSGet.pyproject

---
+++ resources/translations/nusget_ko.ts
@@ -0,0 +1,318 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="26"/>
+        <source>MainWindow</source>
+        <translation>메인윈도우</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="43"/>
+        <source>Available Titles</source>
+        <translation>사용 가능한 타이틀</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="72"/>
+        <source>Wii</source>
+        <translation>Wii</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="103"/>
+        <source>vWii</source>
+        <translation>vWii</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="134"/>
+        <source>DSi</source>
+        <translation>DSi</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="174"/>
+        <source>Title ID</source>
+        <translation>타이틀 ID</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="181"/>
+        <source>v</source>
+        <translation>v</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="194"/>
+        <source>Version</source>
+        <translation>버전</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="201"/>
+        <source>Console:</source>
+        <translation>콘솔:</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="237"/>
+        <source>Start Download</source>
+        <translation>다운로드 시작</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="250"/>
+        <source>Run Script</source>
+        <translation>스크립트 실행</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="277"/>
+        <source>General Settings</source>
+        <translation>일반 설정</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="308"/>
+        <source>Pack installable archive (WAD/TAD)</source>
+        <translation>설치 가능한 아카이브 (WAD/TAD) 팩</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="323"/>
+        <source>File Name</source>
+        <translation>파일 이름</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="357"/>
+        <source>Keep encrypted contents</source>
+        <translation>암호화된 내용 보관</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="393"/>
+        <source>Create decrypted contents (*.app)</source>
+        <translation>복호화된 콘텐츠 (*.app) 생성</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="432"/>
+        <source>Use local files, if they exist</source>
+        <translation>로컬 파일이 있으면 사용</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="477"/>
+        <source>Use the Wii U NUS (faster, only effects Wii/vWii)</source>
+        <translation>Wii U NUS 사용(더 빠르고 Wii/vWii에만 효과 있음)</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="575"/>
+        <source>vWii Title Settings</source>
+        <translation>vWii 타이틀 설정</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="609"/>
+        <source>Re-encrypt title using the Wii Common Key</source>
+        <translation>Wii 공통 키를 사용하여 타이틀을 다시 암호화</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="666"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;meta charset=&quot;utf-8&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+hr { height: 1px; border-width: 0; }
+li.unchecked::marker { content: &quot;\2610&quot;; }
+li.checked::marker { content: &quot;\2612&quot;; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Noto Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;meta charset=&quot;utf-8&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+hr { height: 1px; border-width: 0; }
+li.unchecked::marker { content: &quot;\2610&quot;; }
+li.checked::marker { content: &quot;\2612&quot;; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Noto Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Noto Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="vanished">&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="264"/>
+        <source>No Output Selected</source>
+        <translation>선택된 출력 없음</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="265"/>
+        <source>You have not selected any format to output the data in!</source>
+        <translation>데이터를 출력할 형식을 선택하지 않았습니다!</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="267"/>
+        <source>Please select at least one option for how you would like the download to be saved.</source>
+        <translation>다운로드를 저장할 방법을 하나 이상 선택하세요.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="298"/>
+        <source>Invalid Title ID</source>
+        <translation>잘못된 제목 ID</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="299"/>
+        <source>The Title ID you have entered is not in a valid format!</source>
+        <translation>입력한 타이틀 ID의 형식이 올바르지 않습니다!</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="301"/>
+        <source>Title IDs must be 16 digit strings of numbers and letters. Please enter a correctly formatted Title ID, or select one from the menu on the left.</source>
+        <translation>타이틀 ID는 숫자와 문자로 구성된 16자리 문자열이어야 합니다. 올바르게 포맷된 타이틀 ID를 입력하거나 왼쪽 메뉴에서 하나를 선택하세요.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="303"/>
+        <source>Title ID/Version Not Found</source>
+        <translation>타이틀 ID/버전을 찾을 수 없음</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="304"/>
+        <source>No title with the provided Title ID or version could be found!</source>
+        <translation>제공된 타이틀 ID 또는 버전으로 제목을 찾을 수 없습니다!</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="306"/>
+        <source>Please make sure that you have entered a valid Title ID, or selected one from the title database, and that the provided version exists for the title you are attempting to download.</source>
+        <translation>유효한 타이틀 ID를 입력했는지 또는 타이틀 데이터베이스에서 선택했는지, 그리고 다운로드하려는 타이틀에 대해 제공된 버전이 있는지 확인하세요.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="308"/>
+        <source>Content Decryption Failed</source>
+        <translation>콘텐츠 복호화 실패</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="309"/>
+        <source>Content decryption was not successful! Decrypted contents could not be created.</source>
+        <translation>콘텐츠 복호화가 성공하지 못했습니다! 복호화된 콘텐츠를 만들 수 없습니다.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="312"/>
+        <source>Your TMD or Ticket may be damaged, or they may not correspond with the content being decrypted. If you have checked &quot;Use local files, if they exist&quot;, try disabling that option before trying the download again to fix potential issues with local data.</source>
+        <translation>TMD 또는 티켓이 손상되었거나 복호화되는 콘텐츠와 일치하지 않을 수 있습니다. &quot;로컬 파일이 있으면 사용&quot;을 체크한 경우, 로컬 데이터와 관련된 잠재적인 문제를 해결하기 위해 다시 다운로드를 시도하기 전에 해당 옵션을 비활성화해 보세요.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="315"/>
+        <source>Ticket Not Available</source>
+        <translation>사용 가능한 티켓이 아님</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="316"/>
+        <source>No Ticket is Available for the Requested Title!</source>
+        <translation>요청한 타이틀에 대한 티켓이 없습니다!</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="319"/>
+        <source>A ticket could not be downloaded for the requested title, but you have selected &quot;Pack installable archive&quot; or &quot;Create decrypted contents&quot;. These options are not available for titles without a ticket. Only encrypted contents have been saved.</source>
+        <translation>요청한 타이틀에 대한 티켓을 다운로드할 수 없지만 &quot;설치 가능한 아카이브 팩&quot; 또는 &quot;암호 해독된 콘텐츠 생성&quot;을 선택했습니다. 이러한 옵션은 티켓이 없는 타이틀에는 사용할 수 없습니다. 암호화된 콘텐츠만 저장되었습니다.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="321"/>
+        <source>Unknown Error</source>
+        <translation>알 수 없는 오류</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="322"/>
+        <source>An Unknown Error has Occurred!</source>
+        <translation>알 수 없는 오류가 발생했습니다!</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="324"/>
+        <source>Please try again. If this issue persists, please open a new issue on GitHub detailing what you were trying to do when this error occurred.</source>
+        <translation>다시 시도하세요. 이 문제가 지속되면 GitHub에서 새 이슈를 열어 이 오류가 발생했을 때 무엇을 하려고 했는지 자세히 설명하세요.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="364"/>
+        <source>Open NUS script</source>
+        <translation>NUS 스크립트 열기</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="364"/>
+        <source>NUS Scripts (*.nus *.txt)</source>
+        <translation>NUS 스크립트 (*.nus *.txt)</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="371"/>
+        <location filename="../../NUSGet.py" line="386"/>
+        <location filename="../../NUSGet.py" line="389"/>
+        <location filename="../../NUSGet.py" line="397"/>
+        <location filename="../../NUSGet.py" line="415"/>
+        <location filename="../../NUSGet.py" line="422"/>
+        <source>Script Failure</source>
+        <translation>스크립트 실패</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="371"/>
+        <source>Failed to open the script.</source>
+        <translation>스크립트를 열 수 없습니다.</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="85"/>
+        <source>NUSGet v{nusget_version}
+Developed by NinjaCheetah
+Powered by libWiiPy {libwiipy_version}
+DSi support provided by libTWLPy {libtwlpy_version}
+
+Select a title from the list on the left, or enter a Title ID to begin.
+
+Titles marked with a checkmark are free and have a ticket available, and can be decrypted and/or packed into a WAD or TAD. Titles with an X do not have a ticket, and only their encrypted contents can be saved.
+
+Titles will be downloaded to a folder named &quot;NUSGet&quot; inside your downloads folder.</source>
+        <translation>NUSGet v{nusget_version}
+개발자 : NinjaCheetah
+libWiiPy {libwiipy_version}에서 제공
+DSi 지원 : libTWLPy {libtwlpy_version}에서 제공
+
+왼쪽 목록에서 타이틀을 선택하거나 타이틀 ID를 입력하여 시작하세요.
+
+체크 표시가 있는 타이틀은 무료이며 티켓을 사용할 수 있으며, WAD 또는 TAD에 복호화 및/또는 패킹할 수 있습니다. X가 있는 타이틀은 티켓이 없으며 암호화된 콘텐츠만 저장할 수 있습니다.
+
+타이틀은 다운로드 폴더 내의 &quot;NUSBet&quot;이라는 폴더에 다운로드됩니다.</translation>
+    </message>
+    <message>
+        <location filename="../../qt/ui/MainMenu.ui" line="519"/>
+        <source>Apply patches to IOS (Applies to WADs only)</source>
+        <translation>IOS에 패치 적용 (WAD에만 적용)</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="184"/>
+        <source>NUSGet Update Available</source>
+        <translation>NUSGet 업데이트 가능</translation>
+    </message>
+    <message>
+        <location filename="../../NUSGet.py" line="185"/>
+        <source>There&apos;s a newer version of NUSGet available!</source>
+        <translation>NUSBet의 새로운 버전이 나왔습니다!</translation>
+    </message>
+    <message>
+        <location filename="../../modules/core.py" line="26"/>
+        <source>
+
+Could not check for updates.</source>
+        <translation>
+
+업데이트를 확인할 수 없습니다.</translation>
+    </message>
+    <message>
+        <location filename="../../modules/core.py" line="34"/>
+        <source>
+
+There&apos;s a newer version of NUSGet available!</source>
+        <translation>
+
+NUSBet의 새로운 버전이 나왔습니다!</translation>
+    </message>
+    <message>
+        <location filename="../../modules/core.py" line="36"/>
+        <source>
+
+You&apos;re running the latest release of NUSGet.</source>
+        <translation>
+
+NUSBet의 최신 릴리스를 실행하고 있습니다.</translation>
+    </message>
+</context>
+</TS>
